634e525c4907ba5ff1cf7e319c5e250ab368788a
Emmanuel Camacho <emmanuel.camachod@gmail.com>
2023-07-28 00:13:14 -0400

    Add id model task

--- Model/Task.cs
+++ Model/Task.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
-    internal class Task
+    public class Task 
     {
-
+        [Key]
+        public int Id { get; set; }
     }
 }
